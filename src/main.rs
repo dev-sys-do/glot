27912cf5ed7e861201e42a08f847842373206d32
Samuel Ortiz <sameo@rivosinc.com>
2025-04-03 00:07:26 +0200

    glot: Line parsing function
    Introduce Rust functions.
    
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {
-    let line = "10 LET C = 4 + 2";
-    let chars = line.chars();
-
+fn line_parse(line: &str) {
     println!("{}", line);
+
+    let chars = line.chars();
     for c in chars {
         println!("{}", c);
     }
+}
+
+fn main() {
+    let line = "10 LET C = 4 + 2";
+    line_parse(line);
 }
