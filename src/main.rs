f57187ac253248c2f93b864250d8616575962488
Samuel Ortiz <sameo@rivosinc.com>
2025-04-03 23:30:18 +0200

    glot: Initial identifiers and keywords parsing
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ pub enum Token {
 
     // Variable
     // glot only supports single character variables
-    Identifier(char),
+    Identifier(String),
 
     // Literals
     Number(u64),
@@ -21,34 +21,77 @@ pub enum Token {
 #[derive(Debug)]
 pub enum Error {
     InvalidCharacter(char),
+    InvalidIdentifier(String),
 }
 
 fn line_parse(line: &str) -> Result<Vec<Token>, Error> {
     println!("{}", line);
 
     let mut tokens = Vec::new();
+    let mut chars = line.chars().peekable();
 
-    let chars = line.chars();
-    for c in chars {
+    while let Some(&c) = chars.peek() {
         match c {
             ' ' | '\t' | '\r' | '\n' => {
                 println!("\"{}\"", c);
+
+                // Move the iterator forward
+                chars.next();
             }
 
             '+' | '-' | '*' | '/' => {
                 println!("Operator {}", c);
+
+                // Move the iterator forward
+                chars.next();
             }
 
             '=' => {
                 tokens.push(Token::Equals);
+
+                // Move the iterator forward
+                chars.next();
             }
 
             '0'..='9' => {
                 println!("Digit: {}", c);
+
+                // Move the iterator forward
+                chars.next();
             }
 
             'A'..='Z' => {
-                println!("Letter {}", c);
+                // Identifier for keyword (LET, etc) or variable
+                let mut ident = String::new();
+
+                // Start peeking into the character stream
+                while let Some(&ch) = chars.peek() {
+                    // Exit the loop as soon as the next character is *not*
+                    // an upper case letter
+                    if !ch.is_ascii_uppercase() {
+                        break;
+                    }
+
+                    // Accumulate upper case letters into the identifier
+                    ident.push(ch);
+
+                    // Move the iterator forward
+                    chars.next();
+                }
+
+                // Check if it's a keyword or variable
+                match ident.as_str() {
+                    "LET" => tokens.push(Token::KeywordLet),
+                    _ => {
+                        // If not a keyword, check if it's a valid single-char variable
+                        if ident.len() == 1 {
+                            tokens.push(Token::Identifier(ident));
+                        } else {
+                            // Multi-char variable is an error
+                            return Err(Error::InvalidIdentifier(ident));
+                        }
+                    }
+                }
             }
 
             _ => {
