6c50feb4145bf26cb1a6d44c4c2a7c3ca9a286a6
Samuel Ortiz <sameo@rivosinc.com>
2025-04-04 05:49:00 +0200

    glot: Add a GlotLine structure
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- src/main.rs
+++ src/main.rs
@@ -31,116 +31,119 @@ pub enum Error {
     InvalidNumber(String),
 }
 
-fn tokenize(line: &str) -> Result<Vec<Token>, Error> {
-    println!("{}", line);
+#[derive(Debug, Clone)]
+struct GlotLine {
+    tokens: Vec<Token>,
+}
 
-    let mut tokens = Vec::new();
-    let mut chars = line.chars().peekable();
+impl GlotLine {
+    pub fn new(line: &str) -> Result<Self, Error> {
+        let mut tokens = Vec::new();
+        let mut chars = line.chars().peekable();
 
-    while let Some(&c) = chars.peek() {
-        match c {
-            ' ' | '\t' | '\r' | '\n' => {
-                println!("\"{}\"", c);
-
-                // Move the iterator forward
-                chars.next();
-            }
-
-            '+' => {
-                tokens.push(Token::OperatorPlus);
-                chars.next();
-            }
-            '-' => {
-                tokens.push(Token::OperatorMinus);
-                chars.next();
-            }
-            '*' => {
-                tokens.push(Token::OperatorMultiply);
-                chars.next();
-            }
-            '/' => {
-                tokens.push(Token::OperatorDivide);
-                chars.next();
-            }
-            '=' => {
-                tokens.push(Token::Equals);
-                chars.next();
-            }
-
-            '0'..='9' => {
-                // Build the string representing the number
-                let mut num_str = String::new();
-
-                // Start peeking into the character stream
-                while let Some(&ch) = chars.peek() {
-                    // Exit the loop as soon as the next character is *not* a digit
-                    if !ch.is_ascii_digit() {
-                        break;
-                    }
-
-                    // Accumulate digits into the number string
-                    num_str.push(ch);
-                    chars.next();
-                }
-
-                // Check that this is a valid number
-                match num_str.parse::<u64>() {
-                    Ok(num) => tokens.push(Token::Number(num)),
-                    Err(_) => return Err(Error::InvalidNumber(num_str)),
-                }
-            }
-
-            'A'..='Z' => {
-                // Identifier for keyword (LET, etc) or variable
-                let mut ident = String::new();
-
-                // Start peeking into the character stream
-                while let Some(&ch) = chars.peek() {
-                    // Exit the loop as soon as the next character is *not*
-                    // an upper case letter
-                    if !ch.is_ascii_uppercase() {
-                        break;
-                    }
-
-                    // Accumulate upper case letters into the identifier
-                    ident.push(ch);
-
+        while let Some(&c) = chars.peek() {
+            match c {
+                ' ' | '\t' | '\r' | '\n' => {
                     // Move the iterator forward
                     chars.next();
                 }
 
-                // Check if it's a keyword or variable
-                match ident.as_str() {
-                    "LET" => tokens.push(Token::KeywordLet),
-                    "PRINT" => tokens.push(Token::KeywordPrint),
-                    "END" => tokens.push(Token::KeywordEnd),
+                '+' => {
+                    tokens.push(Token::OperatorPlus);
+                    chars.next();
+                }
+                '-' => {
+                    tokens.push(Token::OperatorMinus);
+                    chars.next();
+                }
+                '*' => {
+                    tokens.push(Token::OperatorMultiply);
+                    chars.next();
+                }
+                '/' => {
+                    tokens.push(Token::OperatorDivide);
+                    chars.next();
+                }
+                '=' => {
+                    tokens.push(Token::Equals);
+                    chars.next();
+                }
 
-                    _ => {
-                        // If not a keyword, check if it's a valid single-char variable
-                        if ident.len() == 1 {
-                            tokens.push(Token::Identifier(ident));
-                        } else {
-                            // Multi-char variable is an error
-                            return Err(Error::InvalidIdentifier(ident));
+                '0'..='9' => {
+                    // Build the string representing the number
+                    let mut num_str = String::new();
+
+                    // Start peeking into the character stream
+                    while let Some(&ch) = chars.peek() {
+                        // Exit the loop as soon as the next character is *not* a digit
+                        if !ch.is_ascii_digit() {
+                            break;
+                        }
+
+                        // Accumulate digits into the number string
+                        num_str.push(ch);
+                        chars.next();
+                    }
+
+                    // Check that this is a valid number
+                    match num_str.parse::<u64>() {
+                        Ok(num) => tokens.push(Token::Number(num)),
+                        Err(_) => return Err(Error::InvalidNumber(num_str)),
+                    }
+                }
+
+                'A'..='Z' => {
+                    // Identifier for keyword (LET, etc) or variable
+                    let mut ident = String::new();
+
+                    // Start peeking into the character stream
+                    while let Some(&ch) = chars.peek() {
+                        // Exit the loop as soon as the next character is *not*
+                        // an upper case letter
+                        if !ch.is_ascii_uppercase() {
+                            break;
+                        }
+
+                        // Accumulate upper case letters into the identifier
+                        ident.push(ch);
+
+                        // Move the iterator forward
+                        chars.next();
+                    }
+
+                    // Check if it's a keyword or variable
+                    match ident.as_str() {
+                        "LET" => tokens.push(Token::KeywordLet),
+                        "PRINT" => tokens.push(Token::KeywordPrint),
+                        "END" => tokens.push(Token::KeywordEnd),
+
+                        _ => {
+                            // If not a keyword, check if it's a valid single-char variable
+                            if ident.len() == 1 {
+                                tokens.push(Token::Identifier(ident));
+                            } else {
+                                // Multi-char variable is an error
+                                return Err(Error::InvalidIdentifier(ident));
+                            }
                         }
                     }
                 }
-            }
 
-            _ => {
-                return Err(Error::InvalidCharacter(c));
+                _ => {
+                    return Err(Error::InvalidCharacter(c));
+                }
             }
         }
-    }
 
-    Ok(tokens)
+        Ok(GlotLine { tokens })
+    }
 }
 
 fn main() -> Result<(), Error> {
     let line = "10 LET C = 4 + 2";
-    let tokens = tokenize(line)?;
+    let glot_line = GlotLine::new(line)?;
 
-    println!("Tokens: {:?}", tokens);
+    println!("Tokens: {:?}", glot_line.tokens);
 
     Ok(())
 }
